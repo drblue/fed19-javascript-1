d5fd8d6c2d81ea2edf66daca258c07b781f7edf6
Johan Nordström <johan@digitalvillage.se>
2019-11-04 13:29:49 +0100

    Datatypes, strings, variables and constants.

---
+++ day01/script.js
@@ -0,0 +1,37 @@
+// SOME ANNOYING ALERT BOXES
+/*
+alert('Hello');
+alert('Such Script');
+alert('Much Wow');
+*/
+
+// LOG ALL THE THINGS
+// console.log('Hello sneaky person who looks at my code');
+
+/*
+let name = 'John Doe';
+let age = 42;
+
+console.log(name);
+
+age = "fyrtiotvå";
+console.log(age);
+
+age = 84;
+console.log(age);
+
+// alert('hello')
+*/
+
+let student_name = "Tony Stark"; // snake_case
+let points = 42;
+
+points = 80;
+
+const maxPointsExam = 84;  // camelCase
+maxpoints = 80;
+
+console.log(points);
+console.log(maxpoints);
+
+var student = "John Doe";
