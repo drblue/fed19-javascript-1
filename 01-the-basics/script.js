53053d327006b2bf1831cfa4ecb315f83bcfb2c5
Johan Nordström <johan@digitalvillage.se>
2019-11-06 14:47:01 +0100

    Booleans and comparissons.

--- 01-the-basics/script.js
+++ 01-the-basics/script.js
@@ -1,6 +1,44 @@
-let points = null;
-console.log(points, points + 3, `Your points are: ${points}`);
+// booleans and comparators
+/*
+// console.log(true, false, 'true', 'false');
 
-let undefined_points;
-console.log(undefined_points, undefined_points + 3, `Your undefined points are: ${undefined_points}`);
+// examples of booleans
+let superheros = [
+	"Tony Stark",
+	"The Hulk",
+	"Thor",
+	"Black Widow",
+	"Captain America",
+	"Loki"
+];
 
+// let result = superheros.includes("loki");
+console.log("Did loki exist as a superhero?", superheros.includes("loki"));
+
+let phone = "1-800-HERO4RENT";
+let is_international = phone.includes('+');
+let is_free = phone.includes('555');
+console.log("Phone number is international:", is_international);
+console.log("Phone number is free to call:", is_free);
+*/
+
+// comparissons
+/*
+let points = 28;
+const PASSED = 26;
+const PASSED_WITH_EXCELLENCE = 33;
+const MAX_POINTS = 40;
+
+console.log(points == PASSED);  // 26 == 26: true
+console.log(points == PASSED_WITH_EXCELLENCE);  // 26 == 33: false
+console.log(points != PASSED);  // 27 != 26: true
+
+console.log(points > PASSED); // 28 > 26: true
+console.log(points < PASSED); // 28 < 26: false
+
+console.log(points >= PASSED); // 28 >= 26: true
+*/
+
+let name = "Thor"; // L = 76
+console.log(name == "Loki"); // false
+console.log(name != "Loki"); // true
