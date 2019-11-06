5c443f9c85cf2eac302bb958eebe0cc94b50fc87
Johan Nordström <johan@digitalvillage.se>
2019-11-06 10:49:43 +0100

    Numbers.

--- 01-the-basics/script.js
+++ 01-the-basics/script.js
@@ -1,5 +1,46 @@
-let msg = "You have 23 likes.";
-// msg = msg.replace('likes', 'thumbs-up');
+/*
+let radius = 20;
+const PI = 3.1415;
 
-console.log(msg.replace('likes', 'thumbs-up'));
-console.log(msg);
+const EXAM_MAX_POINTS = 40;
+let points = 20;
+
+// math operators: +, -, *, /, **, % (modulus, mod)
+
+// console.log(radius % 3);
+console.log(radius * radius * PI);
+console.log(radius ** 2 * PI);
+
+let result = 5 * (10 - 3)**2;
+*/
+
+// let likes = 23;
+// console.log(likes + 1); // 24
+// console.log(likes); // 23
+
+/**
+ * math shorthands
+*/
+
+// likes = likes + 1; // 24
+// likes++;
+
+// likes = likes - 1;
+// likes--;
+
+// console.log(likes); // 25
+
+// likes = likes + 5;
+// likes += 5;
+// likes -= 8;
+// likes *= 3;
+// likes /= 2;
+
+// let welcome_msg = 'You have 23 likes!' - 1;
+// console.log(welcome_msg);
+
+console.log(15 * 'hello this is dog');
+console.log(5 / 'hello this is dog');
+console.log('hello this is dog' - 8);
+console.log('hello this is dog' + 42); // ???
+console.log(1337 + 'hello this is dog'); // ???
