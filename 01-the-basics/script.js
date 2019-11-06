f31bb7e34367e1ad49f25460c79b4e019b6cd38e
Johan Nordström <johan@digitalvillage.se>
2019-11-06 09:47:58 +0100

    Replace all the things.

--- 01-the-basics/script.js
+++ 01-the-basics/script.js
@@ -1,12 +1,5 @@
-let email = "jn@thehiveresistance.com";
-email = "some@dude.org";
-email = "bgates@microsoft.com";
-email = "some.brittish.dude@bt.co.uk";
-email = "hi.mate@at.co.au";
+let msg = "You have 23 likes.";
+// msg = msg.replace('likes', 'thumbs-up');
 
-let indexOfAt = email.indexOf('@');
-let indexOfLastDot = email.lastIndexOf('.');
-let emailLength = email.length;
-let domain = email.slice(indexOfAt + 1, indexOfLastDot);
-
-console.log("Domain without topdomain for email " + email + " is " + domain);
+console.log(msg.replace('likes', 'thumbs-up'));
+console.log(msg);
