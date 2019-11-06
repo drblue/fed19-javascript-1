e37584dbbe7908f205a63523762b35993a7451d4
Johan Nordström <johan@digitalvillage.se>
2019-11-06 09:35:29 +0100

    Get top domain from email variable.

--- 01-the-basics/script.js
+++ 01-the-basics/script.js
@@ -1,67 +1,11 @@
-// SOME ANNOYING ALERT BOXES
-/*
-alert('Hello');
-alert('Such Script');
-alert('Much Wow');
-*/
-
-// LOG ALL THE THINGS
-// console.log('Hello sneaky person who looks at my code');
-
-/*
-let name = 'John Doe';
-let age = 42;
-
-console.log(name);
-
-age = "fyrtiotvå";
-console.log(age);
-
-age = 84;
-console.log(age);
-
-// alert('hello')
-*/
-
-/*
-let student_name; // snake_case
-student_name = 'Tony Stark';
-
-let points = 42;
-
-const maxPointsExam = 80;  // camelCase
-let welcome_msg = "Welcome";
-let alert_msg = welcome_msg + " " + student_name;
-
-console.log(welcome_msg);
-*/
-
 let email = "jn@thehiveresistance.com";
 email = "some@dude.org";
 email = "bgates@microsoft.com";
-
 email = "some.brittish.dude@bt.co.uk";
+email = "hi.mate@at.co.au";
 
-let index_of_at_symbol = email.indexOf('@'); // 2
-let length_of_email = email.length; // 24
-let email_domain = email.slice(index_of_at_symbol+1, length_of_email); // slice(3, 23)
+let indexOfLastDot = email.lastIndexOf('.');
+let emailLength = email.length;
+let topDomain = email.slice(indexOfLastDot + 1);
 
-console.log("Email domain is " + email_domain);
-
-/*
-console.log("Email is " + email);
-console.log("Email is " + email.length + " chars long");
-
-console.log("First char in email is " + email[0]);
-console.log("Third char in email is " + email[2]);
-
-console.log("Last 'e' exists at index " + email.lastIndexOf('e'));
-
-console.log("Email in ALL CAPS is " + email.toUpperCase());
-console.log("Email is " + email);
-
-console.log("Slice of email is " + email.slice(3, 4));
-
-console.log("The 4 first chars in email is " + email.substr(0, 4));
-console.log("The 4 chars from index 3 in email is " + email.substr(3, 4));
-*/
+console.log("Topdomain for email " + email + " is " + topDomain);
