4f1632998533a04496b187d468d23ac59d862495
Johan Nordström <johan@digitalvillage.se>
2019-11-08 11:23:40 +0100

    For-loops.

---
+++ 02-control-flow/script.js
@@ -0,0 +1,18 @@
+/**
+ * control flow
+ */
+
+let avengers = ["Iron Man", "Black Widow", "Captain America", "Hulk", "Thor", "Spider-Man"]; // 6
+//               0,          1,             2,                 3,      4,      5
+
+console.log("There are " + avengers.length + " avengers");
+
+for (let i = 0; i < avengers.length; i++) {
+	console.log("Hello avenger", avengers[i]);
+}
+
+for (let i = 1; i <= 4; i++) {
+	console.log("Look ma I can counts", i);
+}
+
+console.log("Det var allt för de 25 ørena");
