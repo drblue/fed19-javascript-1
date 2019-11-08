4d526dfadc254e56a7b89377c96267c5d7b37df6
Johan Nordström <johan@digitalvillage.se>
2019-11-08 14:27:52 +0100

    Logical NOT.

--- 02-control-flow/script.js
+++ 02-control-flow/script.js
@@ -1,18 +1,11 @@
 /**
- * logical operators:
- * OR ||
- * AND &&
+ * logical NOT
  */
 
-let username = "johan";
-// password can be 8-16 chars long.
-let password = "asdf";
-let fml = true;
+// const user = "Johan";
+const user = false;
 
-let points = (8 + 2) * 4;
-
-if (password.length >= 8 || (password === "admin" && username === "johan") || fml) {
-	console.log("Password is MEGA-SECURE and between 8-16 chars, which is perfect.");
-} else {
-	console.log("Please choose another password.");
+if (!user) {
+	console.log("Please log in.");
 }
+
