2a002bfeb91088f451fe86f5b09e40f7f176b94f
Johan Nordström <johan@digitalvillage.se>
2019-11-08 10:41:01 +0100

    Data types and type conversion.

--- 01-the-basics/script.js
+++ 01-the-basics/script.js
@@ -1,18 +1,36 @@
 /**
- * loose vs strict comparison
+ * data types and type conversion
  */
 
+/*
 let points = 25;
-console.log("points", points);
+console.log("points", points, typeof points);
 
-// loose comparison (different data types can still be equal)
-console.log("points == 25", points == 25);
-console.log("points == '25'", points == '25');
-console.log("points != 25", points != 25);
-console.log("points != '25'", points != '25');
+let name = "Johan";
+console.log("name", name, typeof name);
 
-// strict comparison (compares both value and data type)
-console.log("points === 25", points === 25);
-console.log("points === '25'", points === '25');
-console.log("points !== 25", points !== 25);
-console.log("points !== '25'", points !== '25');
+let ironman = 'Tony Stark';
+console.log("ironman", ironman, typeof ironman);
+
+let strPoints = '25';
+addedPoints = Number(strPoints) + 1;
+console.log("addedPoints", addedPoints, typeof addedPoints);
+
+let numPoints = Number(strPoints);
+console.log("numPoints", numPoints, typeof numPoints);
+*/
+
+// let result = Number('25x');
+// console.log(result);
+
+// let points = 25;
+// let result = String(points);
+// console.log(result, typeof result);
+
+// let result = Boolean(100); // true
+// let result = Boolean(0); // false
+// let result = Boolean(-100); // true
+// let result = Boolean('hello this is dog'); // true
+let result = Boolean(''); // false
+
+console.log(result, typeof result);
