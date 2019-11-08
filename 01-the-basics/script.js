0c97c6f2b6dab8f9092e187c50dd76adabf48fad
Johan Nordström <johan@digitalvillage.se>
2019-11-08 10:21:27 +0100

    Loose vs strict comparison.

--- 01-the-basics/script.js
+++ 01-the-basics/script.js
@@ -1,44 +1,18 @@
-// booleans and comparators
-/*
-// console.log(true, false, 'true', 'false');
+/**
+ * loose vs strict comparison
+ */
 
-// examples of booleans
-let superheros = [
-	"Tony Stark",
-	"The Hulk",
-	"Thor",
-	"Black Widow",
-	"Captain America",
-	"Loki"
-];
+let points = 25;
+console.log("points", points);
 
-// let result = superheros.includes("loki");
-console.log("Did loki exist as a superhero?", superheros.includes("loki"));
+// loose comparison (different data types can still be equal)
+console.log("points == 25", points == 25);
+console.log("points == '25'", points == '25');
+console.log("points != 25", points != 25);
+console.log("points != '25'", points != '25');
 
-let phone = "1-800-HERO4RENT";
-let is_international = phone.includes('+');
-let is_free = phone.includes('555');
-console.log("Phone number is international:", is_international);
-console.log("Phone number is free to call:", is_free);
-*/
-
-// comparissons
-/*
-let points = 28;
-const PASSED = 26;
-const PASSED_WITH_EXCELLENCE = 33;
-const MAX_POINTS = 40;
-
-console.log(points == PASSED);  // 26 == 26: true
-console.log(points == PASSED_WITH_EXCELLENCE);  // 26 == 33: false
-console.log(points != PASSED);  // 27 != 26: true
-
-console.log(points > PASSED); // 28 > 26: true
-console.log(points < PASSED); // 28 < 26: false
-
-console.log(points >= PASSED); // 28 >= 26: true
-*/
-
-let name = "Thor"; // L = 76
-console.log(name == "Loki"); // false
-console.log(name != "Loki"); // true
+// strict comparison (compares both value and data type)
+console.log("points === 25", points === 25);
+console.log("points === '25'", points === '25');
+console.log("points !== 25", points !== 25);
+console.log("points !== '25'", points !== '25');
