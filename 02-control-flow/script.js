80f246de6b003aed400f9df7cc34bbfe3b0f4f13
Johan Nordström <johan@digitalvillage.se>
2019-11-08 11:44:19 +0100

    While-loops and Do While-loops..

--- 02-control-flow/script.js
+++ 02-control-flow/script.js
@@ -2,15 +2,23 @@
  * control flow
  */
 
+/*
 let avengers = ["Iron Man", "Black Widow", "Captain America", "Hulk", "Thor", "Spider-Man"]; // 6
-//               0,          1,             2,                 3,      4,      5
 
-console.log("There are " + avengers.length + " avengers");
-
-for (let i = 0; i < 4; i++) {
-	for (let j = 0; j < avengers.length; j++) {
-		console.log("I'm at loop " + i, avengers[j]);
-	}
+for (let i = 0; i < avengers.length; i++) {
+	console.log("Avenger " + i, avengers[i]);
 }
 
-console.log("Det var allt för de 25 ørena");
+console.log("For-loop done");
+*/
+
+let i = 3;
+while (i < 3) {
+	console.log("i is", i);
+	i++;
+}
+
+let j = 0;
+do {
+	console.log("j is", j);
+} while (j < 3);
