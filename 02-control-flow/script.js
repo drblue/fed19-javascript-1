abf0f049572a796ab6df2f1c0d20ce0c28392553
Johan Nordström <johan@digitalvillage.se>
2019-11-12 11:37:10 +0100

    break and continue.

--- 02-control-flow/script.js
+++ 02-control-flow/script.js
@@ -1,11 +1,20 @@
 /**
- * logical NOT
+ * break & continue
  */
 
-// const user = "Johan";
-const user = false;
+let arr = [
+	3, 9, "banan", 42, false
+];
 
-if (!user) {
-	console.log("Please log in.");
+let points = 0;
+for (let i = 0; i < arr.length; i++) {
+	if (typeof arr[i] !== "number") {
+		console.log(`${arr[i]} is not a number, continuing on next item in array`);
+		continue;
+	}
+
+	console.log(`Adding ${arr[i]} to points`);
+	points += arr[i];
 }
 
+console.log("Points are", points);
