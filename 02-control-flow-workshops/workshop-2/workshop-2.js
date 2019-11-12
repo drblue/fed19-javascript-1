860e15185b0b7a2dbeff6e9ba3b8784871362125
Johan Nordström <johan@digitalvillage.se>
2019-11-12 15:16:40 +0100

    Workshop 2.

---
+++ 02-control-flow-workshops/workshop-2/workshop-2.js
@@ -0,0 +1,22 @@
+/**
+ * WORKSHOP 2
+ *
+ * Gissa talet.
+ *
+ * STEG 1
+ * Sätt ett tal i en variabel. Be användaren att gissa talet. Om det är fel, fråga igen. Om det är rätt, visa en alert med ett grattis-meddelande.
+ * Om användaren skriver in talet 0 så ska spelet avslutas.
+ *
+ * STEG 1.5
+ * Berätta för användaren om gissningen är för låg eller för hög. Naturligtvis ska de få gissa igen efter detta.
+ *
+ * STEG 2
+ * Slumpa talet som användaren ska gissa, så att de inte gissar rätt varje gång.
+ *
+ * STEG 3
+ * Spara ner hur många gissningar som krävdes. Visa antalet gissningar när användaren gissat rätt.
+ *
+ * STEG 4
+ * Efter att man gissat rätt så slumpas ett nytt tal fram och spelet startar igen.
+ * Spara en "highscore", dvs hur få gånger som krävts för att gissa rätt. Om användaren gissar rätt på fler gånger, visa "Tyvärr du gissade rätt på ${tries} antal försök men din highscore är ${highscore}". Om användaren gissar rätt på färre gånger, visa "YAY NEW HIGHSCORE! ${highscore}"
+ */
