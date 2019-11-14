65ad6f4e0db9f29b281b5c75210f94bf13b94053
Johan Nordström <johan@digitalvillage.se>
2019-11-14 11:26:07 +0100

    Before refactoring to functions.

---
+++ 03-functions-and-methods/workshop-1/workshop-1.js
@@ -0,0 +1,61 @@
+/**
+ * WORKSHOP
+ *
+ * Skriv kod som kollar att lösenordet i variabeln password har
+ *
+ *    minst 8 tecken varav minst ett specialtecken enligt nedan ✅
+ *    eller har minst 12 tecken och minst en siffra ✅
+ *    eller har minst 16 tecken ✅
+ *
+ * Specialtecken: @, $, %, *, ^, <, >, ?, !, (, ), [, ], {, }, '
+ */
+
+const SPECIAL_CHARS = [
+	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
+];
+
+const NUMBERS = [
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+];
+
+let password;
+// password = "password"; // inte giltigt
+password = "passw?rd"; // giltigt
+// password = "pa$$word"; // giltigt
+// password = "secretpassword"; // inte giltigt
+// password = "secr3tpassword"; // giltigt
+// password = "such-password-much-secure-very-long"; // giltigt
+
+// Check if `password` has at least one special char
+let has_special_char = false;
+for (let i = 0; i < SPECIAL_CHARS.length; i++) {
+	console.log(`Checking if password contains '${SPECIAL_CHARS[i]}'`);
+	if (password.includes(SPECIAL_CHARS[i])) {
+		console.log(`Password contains '${SPECIAL_CHARS[i]}'`);
+		has_special_char = true;
+		break;
+	}
+}
+
+// Check if `password` has at least one number
+let has_number = false;
+for (let i = 0; i < NUMBERS.length; i++) {
+	if (password.includes(NUMBERS[i])) {
+		has_number = true;
+		break;
+	}
+}
+
+// check if `password` matches criteria
+if (password.length >= 8 && has_special_char) {
+	console.log("Password is at least 8 chars and has at least one special char");
+
+} else if (password.length >= 12 && has_number) {
+	console.log("Password is at least 12 chars and has at least one numeric char");
+
+} else if (password.length >= 16) {
+	console.log("Password is at least 16 chars");
+
+} else {
+	console.log("Password is not secure enough, my grandma can crack it!");
+}
