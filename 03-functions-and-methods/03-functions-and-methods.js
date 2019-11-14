79e65616d9ecc968f95cd4532e79b048bd744bce
Johan Nordström <johan@digitalvillage.se>
2019-11-14 10:05:52 +0100

    Math and functions.

--- 03-functions-and-methods/03-functions-and-methods.js
+++ 03-functions-and-methods/03-functions-and-methods.js
@@ -3,18 +3,35 @@
  *
  */
 
-let r;
-let x;
-
 // Function Declaration
-function calcArea(radius) {
-	console.log(`Let's calculate the area of radius ${radius}!`);
-	let area = radius ** 2 * 3.1415;
-	return area;
+/*
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+*/
+
+/*
+// Function Expression
+const getRandomNumber = function(max) {
+	return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+*/
+
+// Arrow Function (Expression)
+/*
+const getRandomNumber = (max) => {
+	return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
 
-const RADIUS = [10, 20, 30, 5, 18, 42];
-for (let i = 0; i < RADIUS.length; i++) {
-	let a = calcArea(RADIUS[i]);
-	console.log(`The area of a circle with radius ${RADIUS[i]} is ${a}.`);
+const getRandomNumber = max => {
+	return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+*/
+
+const getRandomNumber = max => Math.floor(Math.random() * Math.floor(max)) + 1;
+
+
+// Get me 20 random numbers between 1-50, plz
+for (let i = 0; i < 20; i++) {
+	console.log("Random number", getRandomNumber(50));
 }
