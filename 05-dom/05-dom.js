b58c90825256b98c589a84919acb2d53b851a24c
Johan Nordström <johan@digitalvillage.se>
2019-11-15 14:09:30 +0100

    Fun with Forms (template).

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -3,10 +3,3 @@
  *
  */
 
-let div_error = document.querySelector('div.error');
-div_error.children; // children of div.error. N.B.! Is a HTMLCollection and not a NodeList!
-div_error.parentElement; // div.container
-div_error.parentElement.parentElement; // body
-div_error.previousElementSibling; // p.error
-div_error.previousElementSibling.previousElementSibling; // p
-div_error.nextElementSibling; // null
