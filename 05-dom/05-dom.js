4a2f48aaaf78d0dc2df76c27be632caa9f9bfb2f
Johan Nordström <johan@digitalvillage.se>
2019-11-15 12:48:15 +0100

    innerText vs innerHTML.

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -3,32 +3,12 @@
  *
  */
 
-// get by ID
-let link1 = document.getElementById('link1');
-console.log(link1);
+let paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
 
-/*
- * getElementByClassName and getElementsByTagName returns a
- * HTMLCollection which does NOT have a .forEach()-method
- */
-
-// get by classname
-let errors = document.getElementsByClassName('error');
-console.log(errors);
-
-// get by tag
-let links = document.getElementsByTagName('a');
-console.log(links);
-
-
-/*
- * querySelectorAll returns a NodeList which DOES have a
- * .forEach()-method
- */
-
-let queryLinks = document.querySelectorAll('a');
-console.log(queryLinks);
-
-queryLinks.forEach((item) => {
-	console.log(item);
+paragraphs.forEach((paragraph) => {
+	console.log("Hello I am a paragraph", paragraph);
+	console.log("innerText", paragraph.innerText);
+	console.log("innerHTML", paragraph.innerHTML);
+	paragraph.innerHTML = `<em>${paragraph.innerHTML}</em>`;
 });
