5ef3d881eabc2b081ccea1e3d722442af95b1574
Johan Nordström <johan@digitalvillage.se>
2019-11-15 13:16:17 +0100

    Element classes.

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -3,5 +3,8 @@
  *
  */
 
-let link = document.querySelector('#link1');
-link.setAttribute('title', 'Do try googling it');
+let div_error = document.querySelector('div.error');
+console.log(div_error.classList);
+// div_error.classList.add('alert');
+// div_error.classList.remove('alert');
+div_error.classList.toggle('alert'); // adds the class if it does not exist. removes the class if it exists.
