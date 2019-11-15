d82be5257a09949f63f6d5cb7d2c25156c6ad5dd
Johan Nordström <johan@digitalvillage.se>
2019-11-15 11:36:19 +0100

    Other ways to query the DOM.

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -3,14 +3,32 @@
  *
  */
 
-// let h1 = document.querySelector('h1');
-// console.log(h1, typeof h1);
+// get by ID
+let link1 = document.getElementById('link1');
+console.log(link1);
 
-// console.log(h1.innerText);
+/*
+ * getElementByClassName and getElementsByTagName returns a
+ * HTMLCollection which does NOT have a .forEach()-method
+ */
 
-// h1.innerText = "The DOM is DUMB";
+// get by classname
+let errors = document.getElementsByClassName('error');
+console.log(errors);
 
-let paragraphs = document.querySelectorAll('p.error');
-console.log(paragraphs);
-console.log(paragraphs.length);
-console.log(paragraphs[0].innerText);
+// get by tag
+let links = document.getElementsByTagName('a');
+console.log(links);
+
+
+/*
+ * querySelectorAll returns a NodeList which DOES have a
+ * .forEach()-method
+ */
+
+let queryLinks = document.querySelectorAll('a');
+console.log(queryLinks);
+
+queryLinks.forEach((item) => {
+	console.log(item);
+});
