878aa2d8628bf87963b8a626515b83d82c46ec4b
Johan Nordström <johan@digitalvillage.se>
2019-11-15 13:57:25 +0100

    Traversing the DOM.

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -4,6 +4,9 @@
  */
 
 let div_error = document.querySelector('div.error');
-console.log("innerText", div_error.innerText);
-console.log("innerHTML", div_error.innerHTML);
-console.log("textContent", div_error.textContent);
+div_error.children; // children of div.error. N.B.! Is a HTMLCollection and not a NodeList!
+div_error.parentElement; // div.container
+div_error.parentElement.parentElement; // body
+div_error.previousElementSibling; // p.error
+div_error.previousElementSibling.previousElementSibling; // p
+div_error.nextElementSibling; // null
