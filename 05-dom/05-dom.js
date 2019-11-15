7c19146faae0f1b50265a638603bb467b97f40dd
Johan Nordström <johan@digitalvillage.se>
2019-11-15 11:25:37 +0100

    querySelector and querySelectorAll.

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -2,3 +2,15 @@
  * Document Object Model (DOM).
  *
  */
+
+// let h1 = document.querySelector('h1');
+// console.log(h1, typeof h1);
+
+// console.log(h1.innerText);
+
+// h1.innerText = "The DOM is DUMB";
+
+let paragraphs = document.querySelectorAll('p.error');
+console.log(paragraphs);
+console.log(paragraphs.length);
+console.log(paragraphs[0].innerText);
