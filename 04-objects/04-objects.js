dd97710355ce744b2a9fc93035b8f455788d46da
Johan Nordström <johan@digitalvillage.se>
2019-11-15 10:33:22 +0100

    this.

--- 04-objects/04-objects.js
+++ 04-objects/04-objects.js
@@ -1,39 +1,25 @@
 /**
  * OBJECTS
  *
- * All Your Object Belong To Me.
+ * this
  */
-
-let points = 42;
-let pointsCopy = points;
-console.log(`Points: ${points}, pointsCopy: ${pointsCopy}`);
-
-pointsCopy = 1337;
-console.log(`Points: ${points}, pointsCopy: ${pointsCopy}`);
 
 let my_cat = {
 	name: "Mr Fluffles",
 	age: 7,
-	legs: 4
+	legs: 4,
+	speak: () => {
+		// with arrow functions, `this` will reference the context of the caller
+		// with normal functions, `this` will be bound to the context of the object
+		console.log("this from within my_cat", this);
+		console.log("My name is", this.name);
+	}
 };
 
-let my_second_cat = my_cat; // this only copies the reference to the object, not the object itself
-console.log(`My cat's name is: ${my_cat.name}`);
-console.log(`My second cat's name is: ${my_second_cat.name}`);
+let speak = () => {
+	// do something
+}
+speak();
 
-my_second_cat.name = "Ms FLUFFY";
-console.log(`My cat's name is: ${my_cat.name}`);
-console.log(`My second cat's name is: ${my_second_cat.name}`);
-
-
-const my_array = [
-	"Item 1",
-	"Item 2",
-];
-
-my_array.push("Item 3"); // this works because an array is an object and we only store the reference/pointer to the array as a constant.
-console.log(my_array);
-
-my_array = [
-	"My new Item 1",
-];
+console.log("this from the global context", this);
+my_cat.speak();
