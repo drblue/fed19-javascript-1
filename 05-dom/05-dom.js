e0035b061f9f52124bc7fad5a87023ff4a319aa8
Johan Nordström <johan@digitalvillage.se>
2019-11-15 13:35:31 +0100

    textContent gets the content of all elements, not just the visible ones.

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -4,7 +4,6 @@
  */
 
 let div_error = document.querySelector('div.error');
-console.log(div_error.classList);
-// div_error.classList.add('alert');
-// div_error.classList.remove('alert');
-div_error.classList.toggle('alert'); // adds the class if it does not exist. removes the class if it exists.
+console.log("innerText", div_error.innerText);
+console.log("innerHTML", div_error.innerHTML);
+console.log("textContent", div_error.textContent);
