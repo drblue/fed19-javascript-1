2fd8a075d15920e6d33c477a89fdcedb82f04aa0
Johan Nordström <johan@digitalvillage.se>
2019-11-19 12:03:42 +0100

    My Awesomest TODO List.

--- 05-dom/05-dom.js
+++ 05-dom/05-dom.js
@@ -1,31 +1,83 @@
 /**
- * Document Object Model (DOM).
+ * My Awesomest TODO List.
  *
  */
 
-/*
-let listitems = document.querySelectorAll('#list li');
-listitems.forEach(function(listitem) {
-	listitem.addEventListener('click', function(e) {
-		e.target.remove();
-	});
-});
-*/
+let todos = [
+	{ // 0
+		description: "Have class meeting",
+		completed: false,
+	},
+	{ // 1
+		description: "Eat lunch",
+		completed: false,
+	},
+	{ // 2
+		description: "Code",
+		completed: true,
+	},
+	{ // 3
+		description: "Sleep",
+		completed: true,
+	},
+	{ // 4
+		description: "Repeat",
+		completed: false,
+	},
+];
 
-let list = document.querySelector('#list');
-list.addEventListener('click', function(e) {
+let todosEl = document.querySelector('#todos');
+
+const renderTodoList = function() {
+	todosEl.innerHTML = "";
+
+	todos.forEach(function(todo) {
+		let todoEl = document.createElement('li');
+		todoEl.innerText = todo.description;
+
+		// check if todo is completed, if so add class 'completed'
+		if (todo.completed) {
+			todoEl.classList.add('completed');
+		}
+
+		todosEl.append(todoEl);
+	});
+};
+renderTodoList();
+
+// Add click handler for updating completed status
+todosEl.addEventListener('click', function(e) {
 	if (e.target.tagName === "LI") {
-		e.target.classList.toggle('completed');
+		// update completed status for this todo item
+		todos.forEach(function(todo) {
+			if (todo.description === e.target.innerText) {
+				if (todo.completed) {
+					todo.completed = false;
+				} else {
+					todo.completed = true;
+				}
+				// shorthand of above if-statement
+				// todo.completed = !todo.completed;
+
+				// render the updated todo list to DOM
+				renderTodoList();
+			}
+		});
 	}
 });
 
-let createButton = document.querySelector('#create');
-createButton.addEventListener('click', function() {
-	// let newListItem = `<li>A New Item</li>`;
-	// newListItem.addEventListener(); // not workey
-	// list.innerHTML += newListItem;
+// Add click handler for creating a new TODO
+let createNewTodoButton = document.querySelector("#createNewTodo");
+createNewTodoButton.addEventListener('click', function() {
+	let text = prompt("What do you want to add to the TODO list?", "Do Rainman Dance");
 
-	let newListItem = document.createElement('li');
-	newListItem.innerText = prompt('Enter text', 'A New Item');
-	list.append(newListItem);
+	let newTodo = {
+		description: text,
+		completed: false
+	}
+
+	todos.push(newTodo);
+
+	// render the updated todo list to DOM
+	renderTodoList();
 });
