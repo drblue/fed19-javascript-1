334e9e16d25f4b20b119a3630d9553e54bd2d3dd
Johan Nordström <johan@digitalvillage.se>
2019-11-20 14:11:58 +0100

    Supporting sum and just one input field!

--- 06-calc/app.js
+++ 06-calc/app.js
@@ -4,18 +4,35 @@
  */
 
 const calcForm = document.querySelector('#calc-form');
-const firstNumberEl = document.querySelector('#firstNumber');
-const secondNumberEl = document.querySelector('#secondNumber');
+const numberEl = document.querySelector('#number');
 const sumEl = document.querySelector('#sum');
 
+let sum = 0;
+
+// catch when the form is being submitted
 calcForm.addEventListener('submit', function(e) {
 	// stop form from being sent to the server
 	e.preventDefault();
 
-	// get numbers and cast them to numbers
-	const firstNumber = Number(firstNumberEl.value);
-	const secondNumber = Number(secondNumberEl.value);
+	// get the number to calculate and cast it to a number
+	const numberToCalc = Number(numberEl.value);
 
-	const sum = firstNumber + secondNumber;
+	// empty numberEl
+	numberEl.value = "";
+
+	// add `numberToCalc` to whatever is in the `sum` variable
+	// sum = sum + numberToCalc;
+	sum += numberToCalc;
+
+	// set `sumEl`'s innerText to be the value of `sum`
 	sumEl.innerText = sum;
+});
+
+// catch when the form is being reset
+calcForm.addEventListener('reset', function(e) {
+	// reset sum to 0
+	sum = 0;
+
+	// update DOM
+	sumEl.innerText = "0";
 });
