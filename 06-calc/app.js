dad70a9e1220850aa8193fde2619025525c093e4
Johan Nordström <johan@digitalvillage.se>
2019-11-20 14:52:19 +0100

    Just added some styling and made a very helpful helper function.

--- 06-calc/app.js
+++ 06-calc/app.js
@@ -5,9 +5,12 @@
 
 const calcForm = document.querySelector('#calc-form');
 const numberEl = document.querySelector('#number');
-const sumEl = document.querySelector('#sum');
 
 let sum = 0;
+
+const updateSumEl = function(val) {
+	document.querySelector('#sum').innerText = val;
+}
 
 // catch when the form is being submitted
 calcForm.addEventListener('submit', function(e) {
@@ -25,7 +28,7 @@ calcForm.addEventListener('submit', function(e) {
 	sum += numberToCalc;
 
 	// set `sumEl`'s innerText to be the value of `sum`
-	sumEl.innerText = sum;
+	updateSumEl(sum);
 });
 
 // catch when the form is being reset
@@ -34,5 +37,5 @@ calcForm.addEventListener('reset', function(e) {
 	sum = 0;
 
 	// update DOM
-	sumEl.innerText = "0";
+	updateSumEl(sum);
 });
