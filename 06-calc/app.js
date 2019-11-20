63408284c2310db30d0f4d132fe9f760fc64f814
Johan Nordström <johan@digitalvillage.se>
2019-11-20 12:12:53 +0100

    We can calculate!

--- 06-calc/app.js
+++ 06-calc/app.js
@@ -6,19 +6,16 @@
 const calcForm = document.querySelector('#calc-form');
 const firstNumberEl = document.querySelector('#firstNumber');
 const secondNumberEl = document.querySelector('#secondNumber');
+const sumEl = document.querySelector('#sum');
 
 calcForm.addEventListener('submit', function(e) {
 	// stop form from being sent to the server
 	e.preventDefault();
 
-	console.log("Form prevented from being submitted! yay!");
+	// get numbers and cast them to numbers
+	const firstNumber = Number(firstNumberEl.value);
+	const secondNumber = Number(secondNumberEl.value);
 
-	const firstNumberStr = firstNumberEl.value;
-	const secondNumberStr = secondNumberEl.value;
-
-	const firstNumber = Number(firstNumberStr);
-	const secondNumber = Number(secondNumberStr);
-
-	console.log("First number", firstNumber, typeof firstNumber);
-	console.log("Second number", secondNumber, typeof secondNumber);
+	const sum = firstNumber + secondNumber;
+	sumEl.innerText = sum;
 });
