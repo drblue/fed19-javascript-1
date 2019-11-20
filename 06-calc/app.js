095252bbcde8e088170d288a181e4f605b645b4b
Johan Nordström <johan@digitalvillage.se>
2019-11-20 11:33:00 +0100

    First small steps towards a very simple calculator.

--- 06-calc/app.js
+++ 06-calc/app.js
@@ -2,3 +2,23 @@
  * Calc.
  *
  */
+
+const calcForm = document.querySelector('#calc-form');
+const firstNumberEl = document.querySelector('#firstNumber');
+const secondNumberEl = document.querySelector('#secondNumber');
+
+calcForm.addEventListener('submit', function(e) {
+	// stop form from being sent to the server
+	e.preventDefault();
+
+	console.log("Form prevented from being submitted! yay!");
+
+	const firstNumberStr = firstNumberEl.value;
+	const secondNumberStr = secondNumberEl.value;
+
+	const firstNumber = Number(firstNumberStr);
+	const secondNumber = Number(secondNumberStr);
+
+	console.log("First number", firstNumber, typeof firstNumber);
+	console.log("Second number", secondNumber, typeof secondNumber);
+});
