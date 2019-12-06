40978e4b0c3e462f3a395b7c9b4b8d3527f101f3
Johan Nordström <johan@digitalvillage.se>
2019-12-06 15:02:51 +0100

    Working clock using JavaScripts built-in Date class.

--- 11-clock/app.js
+++ 11-clock/app.js
@@ -3,3 +3,47 @@
  *
  */
 
+const clockEl = document.querySelector('#clock');
+const dateEl = document.querySelector('#date');
+const timeEl = document.querySelector('#time');
+
+const zeroPad = num => {
+	if (num < 10) {
+		return "0" + num;
+	}
+	return String(num);
+};
+
+const updateClock = () => {
+	// current date
+	const now = new Date();
+
+	// weekday
+
+	// day of month
+	const dayOfMonth = zeroPad(now.getDate());
+
+	// month
+	const month = zeroPad(now.getMonth() + 1);
+
+	// year
+	const year = now.getFullYear();
+
+	// hour
+	const hours = zeroPad(now.getHours());
+
+	// minute
+	const minutes = zeroPad(now.getMinutes());
+
+	// second
+	const seconds = zeroPad(now.getSeconds());
+
+	const date = `${year}-${month}-${dayOfMonth}`;
+	const time = `${hours}:${minutes}:${seconds}`;
+
+	dateEl.innerHTML = date;
+	timeEl.innerHTML = time;
+}
+
+updateClock();
+const secondTicker = setInterval(updateClock, 1000);
