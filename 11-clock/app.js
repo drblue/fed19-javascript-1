a6e89d9156a2bcf3d809403b92d2d632f3e7bcff
Johan Nordström <johan@digitalvillage.se>
2019-12-06 15:34:55 +0100

    Moment-version.

--- 11-clock/app.js
+++ 11-clock/app.js
@@ -7,42 +7,9 @@ const clockEl = document.querySelector('#clock');
 const dateEl = document.querySelector('#date');
 const timeEl = document.querySelector('#time');
 
-const zeroPad = num => {
-	if (num < 10) {
-		return "0" + num;
-	}
-	return String(num);
-};
-
 const updateClock = () => {
-	// current date
-	const now = new Date();
-
-	// weekday
-
-	// day of month
-	const dayOfMonth = zeroPad(now.getDate());
-
-	// month
-	const month = zeroPad(now.getMonth() + 1);
-
-	// year
-	const year = now.getFullYear();
-
-	// hour
-	const hours = zeroPad(now.getHours());
-
-	// minute
-	const minutes = zeroPad(now.getMinutes());
-
-	// second
-	const seconds = zeroPad(now.getSeconds());
-
-	const date = `${year}-${month}-${dayOfMonth}`;
-	const time = `${hours}:${minutes}:${seconds}`;
-
-	dateEl.innerHTML = date;
-	timeEl.innerHTML = time;
+	dateEl.innerHTML = moment().format('YYYY-MM-DD');
+	timeEl.innerHTML = moment().format('HH:mm:ss');
 }
 
 updateClock();
