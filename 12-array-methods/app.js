21d7150ce84ada7e9ae3400ac500f1dc30b5a105
Johan Nordström <johan@digitalvillage.se>
2019-12-10 13:51:55 +0100

    Array.find()

--- 12-array-methods/app.js
+++ 12-array-methods/app.js
@@ -6,23 +6,61 @@
  * .map()
  * .reduce()
  * .sort()
+ * .find()
  *
  * Method Chaining.
  *
  */
 
-const prices = [20, 10, 15, 25, 30, 40, 80, 5];
-const totalValue = prices.reduce(function(sum, price) {
-	return sum + price;
-}, 0);
+const catnames = [
+	"Picatso",
+	"William Shakespaw",
+	"Mega-bite",
+	"Chairwoman Miao",
+	"Notorious C.A.T",
+];
 
-console.log("totalValue", totalValue);
-
-const valueOfExpensiveProducts = prices.reduce(function(sum, price) {
-	if (price > 30) {
-		return sum + price;
+/*
+let megabite = catnames.find(catname => {
+	if (catname === "Mega-bite") {
+		return true;
 	}
-	return sum;
-}, 0);
+	return false;
+});
+console.log("Found Mega-Bite?", megabite);
+*/
 
-console.log("valueOfExpensiveProducts", valueOfExpensiveProducts);
+const cats = [
+	{ name: "Picatso", age: 8 },
+	{ name: "William Shakespaw", age: 12 },
+	{ name: "Mega-bite", age: 1 },
+	{ name: "Chairwoman Miao", age: 17 },
+	{ name: "Notorious C.A.T", age: 3 },
+];
+
+/*
+const megabite = cats.find(cat => {
+	if (cat.name === "Mega-bite") {
+		return true;
+	}
+	return false;
+});
+const foundCat = cats.find(cat => cat.name === "Chairwoman Miao");
+console.log("Found cat?", foundCat);
+*/
+
+const products = [
+	{sku: 'GOLD-STAR-1', name: 'gold star', price: 20, stock: 3},
+	{sku: 'MUSHROOM-3', name: 'mushroom', price: 40, stock: 0},
+	{sku: 'GREEN-SHELLS', name: 'green shells', price: 30, stock: 1337},
+	{sku: 'BANANANA', name: 'banana skin', price: 10, stock: 2},
+	{sku: 'SHELLS', name: 'red shells', price: 50, stock: 0}
+];
+
+const product = products.find(product => product.sku === 'MUSHROOM-3');
+console.log("Found your product!", product);
+if (product.stock > 0) {
+	alert("Added to your basket");
+} else {
+	alert(`Sorry, we're out of stock on ${product.name}s`);
+}
