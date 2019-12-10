75b2cdfa2395a9d690bd8dd8eac9f061c3174934
Johan Nordström <johan@digitalvillage.se>
2019-12-10 11:38:19 +0100

    Array.reduce()

--- 12-array-methods/app.js
+++ 12-array-methods/app.js
@@ -11,44 +11,18 @@
  *
  */
 
-/*
-const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+const prices = [20, 10, 15, 25, 30, 40, 80, 5];
+const totalValue = prices.reduce(function(sum, price) {
+	return sum + price;
+}, 0);
 
-const blackFridayPrices = [];
-for (let i = 0; i < prices.length; i++) {
-	let price = prices[i];
-	blackFridayPrices.push(price / 2);
-}
+console.log("totalValue", totalValue);
 
-const blackFridayPrices = prices.map(price => price / 2);
-
-console.log("Regular sale prices", prices);
-console.log("Black Friday prices", blackFridayPrices);
-*/
-
-const products = [
-	{name: 'gold star', price: 20},
-	{name: 'mushroom', price: 40},
-	{name: 'green shells', price: 30},
-	{name: 'banana skin', price: 10},
-	{name: 'red shells', price: 50}
-];
-
-// const saleProducts = products.map(product => {
-// 	return {
-// 		name: product.name,
-// 		price: product.price / 2
-// 	};
-// });
-const saleProducts = products.map(product => {
-	if (product.price >= 30) {
-		return {
-			name: product.name,
-			price: product.price / 2
-		};
-	} else {
-		return product;
+const valueOfExpensiveProducts = prices.reduce(function(sum, price) {
+	if (price > 30) {
+		return sum + price;
 	}
-});
-console.log("products", products);
-console.log("saleProducts", saleProducts);
+	return sum;
+}, 0);
+
+console.log("valueOfExpensiveProducts", valueOfExpensiveProducts);
