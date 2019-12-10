9dce78b3b445659bfb72c64e067dbcd224cb4811
Johan Nordström <johan@digitalvillage.se>
2019-12-10 09:51:55 +0100

    Starting and stopping time.

--- 11-clock/app.js
+++ 11-clock/app.js
@@ -6,11 +6,25 @@
 const clockEl = document.querySelector('#clock');
 const dateEl = document.querySelector('#date');
 const timeEl = document.querySelector('#time');
+let secondTicker;
 
 const updateClock = () => {
 	dateEl.innerHTML = moment().format('YYYY-MM-DD');
 	timeEl.innerHTML = moment().format('HH:mm:ss');
 }
 
-updateClock();
-const secondTicker = setInterval(updateClock, 1000);
+const runClock = () => {
+	updateClock();
+	secondTicker = setInterval(updateClock, 1000);
+}
+
+runClock();
+
+clockEl.addEventListener('click', () => {
+	if (secondTicker) {
+		clearInterval(secondTicker);
+		secondTicker = false;
+	} else {
+		runClock();
+	}
+});
