e0d35049801d80b8f4197800c6bfa898b6385410
Johan Nordström <johan@digitalvillage.se>
2019-12-12 14:33:42 +0100

    Asynchronous JavaScript.

---
+++ 14-asynchronous-javascript/app.js
@@ -0,0 +1,20 @@
+/**
+ * Asynchronous JavaScript.
+ *
+ */
+
+console.log(1);
+console.log(2);
+
+setTimeout(() => {
+	console.log("Hi I'm a timer");
+	console.log("And I'm done");
+}, 3000);
+
+// setInterval(() => {
+// 	console.log(Math.random());
+// }, 2000);
+
+console.log(3);
+console.log(4);
+console.log(5);
