ce58c0c49a902e96f2e2e745ef20ed224edc7437
Johan Nordström <johan@digitalvillage.se>
2019-12-16 10:58:45 +0100

    Promise-pets.

--- 15-promises/app.js
+++ 15-promises/app.js
@@ -2,3 +2,126 @@
  * Promises.
  *
  */
+
+/*
+const isWeekend = true;
+
+const getCookies = new Promise((resolve, reject) => {
+	if (isWeekend) {
+		resolve();
+	} else {
+		reject();
+	}
+});
+
+getCookies.then(() => {
+	alert("OMG I LOVE COOKIES");
+}).catch(() => {
+	alert("Nooooes, I'm sad");
+});
+*/
+
+
+const getJSON = (url) => {
+
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest();
+
+		request.addEventListener('readystatechange', () => {
+			if (request.readyState === 4) {
+				if (request.status !== 200) {
+					console.error("Did not get 200 OK in response.", request);
+					reject(`Did not get 200 OK in response from '${url}'.`);
+				} else {
+					console.log(`Success in getting data from '${url}'`);
+					let data = JSON.parse(request.responseText);
+					resolve(data);
+				}
+			}
+		});
+
+		request.open('GET', url);
+		request.send();
+		console.log(`Request for data sent to '${url}'`);
+	});
+};
+
+/**
+ * 1. Get `pets/pets.json`, wait for response. ✅
+ *
+ * 2. Based on response-array of `pets/pets.json` ✅,
+ * make a request to the URL of each item (in parallel).
+ *
+ * 3. When we get a response back for each sub-request,
+ * render the contents to their own UL-element.
+ */
+
+const petsWrapperEl = document.querySelector('#pets-wrapper');
+
+const renderPetList = (title, pets) => {
+	let petsTitleEl = document.createElement("H2");
+	petsTitleEl.innerText = title;
+
+	let petsEl = document.createElement("UL");
+	pets.forEach(pet => {
+		let petEl = document.createElement("LI");
+		petEl.innerHTML = `${pet.name} is ${pet.age} years old`;
+
+		petsEl.append(petEl);
+	});
+
+	petsWrapperEl.append(petsTitleEl);
+	petsWrapperEl.append(petsEl);
+}
+
+console.log("Getting birds..");
+getJSON("pets/birds.json")
+.then(data => {
+	renderPetList("Birds", data);
+
+	return getJSON("pets/cats.json");
+})
+.then(data => {
+	renderPetList("Cats", data);
+
+	return getJSON("pets/dogs.json");
+})
+.then(data => {
+	renderPetList("Dogs", data);
+})
+.catch(err => {
+	alert("Error getting birds/cats");
+});
+
+
+/*
+
+getJSON("pets/birds.json", (err, birds) => {
+	// and render a UL to the DOM for each response 👨🏻‍🎨
+	renderPetList("Birds", birds);
+
+	// get cats
+	getJSON("pets/cats.json", (err, cats) => {
+		if (err) {
+			alert("Error getting cats");
+			return;
+		}
+
+		// and render a UL to the DOM for each response 👨🏻‍🎨
+		renderPetList("Cats", cats);
+
+		// get dogs
+		getJSON("pets/dogs.json", (err, dogs) => {
+			if (err) {
+				alert("Error getting dogs");
+				return;
+			}
+
+			// and render a UL to the DOM for each response 👨🏻‍🎨
+			renderPetList("Dogs", dogs);
+		});
+	});
+});
+*/
+
+console.log("All requests are sent");
