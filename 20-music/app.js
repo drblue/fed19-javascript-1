6b3955c395696a914514908c6fea8459d2d55c65
Johan Nordström <johan@digitalvillage.se>
2019-12-20 10:15:06 +0100

    First search.

--- 20-music/app.js
+++ 20-music/app.js
@@ -3,3 +3,25 @@
  *
  */
 
+const search = async () => {
+	const request = await fetch("https://deezerdevs-deezer.p.rapidapi.com/swqeqweearch?q=sia%20unstoppable", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+			"x-rapidapi-key": "06ebd6b4ffmsh9f40daeb7de6cefp160b3ejsn550347289560"
+		}
+	});
+	if (!request.ok) {
+		throw new Error(`Request was not OK. Status Code returned was: ${request.status}.`);
+	}
+	const response = await request.json();
+	return response;
+}
+
+search()
+	.then(response => {
+		console.log("Got data back!", response);
+	})
+	.catch(err => {
+		console.error("Got an error :(", err);
+	});
