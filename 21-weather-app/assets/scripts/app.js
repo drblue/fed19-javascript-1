d13f36bed373ce9df5c79f3447ebdc428a86f854
Johan Nordström <johan@digitalvillage.se>
2020-01-07 11:51:57 +0100

    Rendering of weather report.

--- 21-weather-app/assets/scripts/app.js
+++ 21-weather-app/assets/scripts/app.js
@@ -3,3 +3,37 @@
  *
  */
 
+const renderCurrentWeather = data => {
+	const template = `
+		<div class="card">
+			<img src="assets/images/forecast-banner.png" class="card-img-top">
+			<div class="card-body">
+				<h5 class="card-title" id="city">${data.name}</h5>
+				<p class="temp">
+					<span id="temperature">${data.main.temp}</span>
+					&deg;C
+				</p>
+				<p class="wind">
+					<span id="windspeed">${data.wind.speed}</span>
+					m/s
+				</p>
+			</div>
+		</div>
+	`;
+
+	document.querySelector('#forecast').innerHTML = template;
+};
+
+document.querySelector('#search-form').addEventListener('submit', e => {
+	e.preventDefault();
+
+	const city = document.querySelector('#query').value;
+
+	if (city.length < 2) {
+		return;
+	}
+
+	getCurrentWeather(city).then(data => {
+		renderCurrentWeather(data);
+	});
+});
--- 21-weather-app/assets/scripts/openweathermap.js
+++ 21-weather-app/assets/scripts/openweathermap.js
@@ -6,3 +6,14 @@ const API_KEY = 'a9f6719e37f20890ebff5d91724dec1f';
 const BASE_URL = 'http://api.openweathermap.org/data/2.5';
 
 // /weather?q=Malmö,se
+
+const getCurrentWeather = async city => {
+	// get weather for `city` from OpenWeatherMap API
+	const response = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
+
+	// convert response from JSON
+	const data = await response.json();
+
+	// return current weather
+	return data;
+};
