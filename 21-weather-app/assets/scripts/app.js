b039262b5779434d72fb715dbf074fced8e7ed73
Johan Nordström <johan@digitalvillage.se>
2020-01-09 10:16:23 +0100

    Improved weather-app according to workshop from Tuesday.

--- 21-weather-app/assets/scripts/app.js
+++ 21-weather-app/assets/scripts/app.js
@@ -9,19 +9,42 @@ const renderAlert = (severity, msg) => {
 };
 
 const renderCurrentWeather = data => {
+	const conditions = data.weather.map(condition => `<li><img src="http://openweathermap.org/img/wn/${condition.icon}@2x.png">${condition.description}</li>`);
+
+	const freshness = moment.unix(data.dt);
+
+	const now = Math.round(Date.now() / 1000);
+
+	// let image = "forecast-banner.png";
+	// if (now > data.sys.sunset || now < data.sys.sunrise) {
+	// 	image = 'night.svg';
+	// } else {
+	// 	image = 'day.svg';
+	// }
+
+	const image = (now > data.sys.sunset || now < data.sys.sunrise)
+		? 'night.svg'
+		: 'day.svg';
+
 	document.querySelector('#forecast').innerHTML = `
 		<div class="card">
-			<img src="assets/images/forecast-banner.png" class="card-img-top">
+			<img src="assets/images/${image}" class="card-img-top">
 			<div class="card-body">
 				<h5 class="card-title" id="city">${data.name}, <span id="country">${data.sys.country}</span></h5>
 				<p class="temp">
 					<span id="temperature">${data.main.temp}</span>
 					&deg;C
 				</p>
+				<p class="humi">
+					<span id="humidity">${data.main.humidity}</span>
+					&percnt; humidity
+				</p>
 				<p class="wind">
 					<span id="windspeed">${data.wind.speed}</span>
 					m/s
 				</p>
+				<ul class="conditions">${conditions.join('')}</ul>
+				<p class="text-muted small">${freshness.fromNow()}</p>
 			</div>
 		</div>
 	`;
