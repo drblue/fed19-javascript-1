f02655611974a1a02ffdd4d9e057597f4b10cd75
Johan Nordström <johan@digitalvillage.se>
2020-01-10 10:37:01 +0100

    Even moar classes.

--- 22-classes/assets/scripts/app.js
+++ 22-classes/assets/scripts/app.js
@@ -5,134 +5,67 @@
  *
  */
 
-class House {
-	constructor(doors, windows, floors) {
-		this.address = "";
-		this.doors = doors;
-		this.windows = windows;
-		this.floors = floors;
-		this.owner = false;
-	}
-	setOwner(name) {
-		this.owner = name;
-	}
-}
-
-const my_house = new House(2, 4, 1);
-my_house.setOwner("Meeeee!");
-
-const neiiiighbour = new House(2, 4, 1);
-neiiiighbour.setOwner("My neighbour");
-
-
-class Product {
-	constructor(name, price, stock) {
-		this.name = name;
-		this.price = price;
-		this.stock = stock;
+class Exam {
+	constructor(student, max_score) {
+		this.student = student;
+		this.max_score = max_score;
+		this.score = 0;
 	}
 
-	addStock(amount) {
-		this.stock += amount;
-	}
-
-	getPrice() {
-		return this.price;
-	}
-
-	getStock() {
-		return this.stock;
-	}
-
-	inStock() {
-		return this.stock > 0;
-	}
-
-	decreaseStock(amount = 1) {
-		this.stock = this.stock - amount;
-	}
-}
-
-class ProductList {
-	constructor() {
-		this.productlist = [];
-	}
-
-	addProduct(product) {
-		this.productlist.push(product);
+	incScore(points) {
+		this.score += points;
 		return this;
 	}
 
-	getProducts() {
-		return this.products;
+	getGrade() {
+		if (this.score / this.max_score >= 0.8) {
+			return 'VG';
+		} else if (this.score / this.max_score >= 0.6) {
+			return 'G';
+		} else {
+			return 'IG';
+		}
 	}
 
-	getProductsInStock() {
-		return this.productlist.filter(product => product.stock > 1);
+	getScore() {
+		return this.score;
 	}
 
-	getProductByName(name) {
-		return this.productlist.find(product => product.name === name);
+	getStudent() {
+		return this.student;
 	}
 }
 
-
-class Basket {
+class ExamList {
 	constructor() {
-		this.products = [];
+		this.exams = [];
 	}
 
-	getProducts() {
-		return this.products;
-	}
-
-	addProduct(product) {
-		product.decreaseStock();
-		this.products.push(product);
+	addExam(exam) {
+		this.exams.push(exam);
 		return this;
 	}
 
-	calcTotal() {
-		return this.products
-			.map(product => product.getPrice())
-			.reduce((sum, price) => sum + price, 0);
+	getExam(student) {
+		return this.exams.find(exam => exam.getStudent() === student);
 	}
 
-	printShoppingInfo() {
-		console.log(`This basket has ${this.products.length} products with a total value of ${this.calcTotal()}`);
+	getExams() {
+		return this.exams;
 	}
 }
 
-const pelles_basket = new Basket();
-const alicias_basket = new Basket();
+const exams = new ExamList();
 
-const bananana = new Product("Bananana", 14.5, 100);
-const bapple = new Product("Bapple", 5, 20);
-const orange = new Product("Orange", 42, 2);
+exams
+	.addExam(new Exam("Elliot", 10))
+	.addExam(new Exam("Maja", 10))
+	.addExam(new Exam("Alicia", 10));
 
-const productlist = new ProductList();
-productlist.addProduct(new Product("Bananana", 14.5, 100))
-	.addProduct(new Product("Bapple", 5, 20))
-	.addProduct(new Product("Orange", 42, 2));
+exams.getExam("Alicia").incScore(4).incScore(0).incScore(3);
+exams.getExam("Elliot").incScore(2).incScore(1).incScore(0);
+exams.getExam("Maja").incScore(3).incScore(1).incScore(4);
 
-pelles_basket
-	.addProduct(bananana)
-	.addProduct(bapple)
-	.addProduct(orange);
-
-pelles_basket.addProduct(bananana)
-.addProduct(bapple)
-.addProduct(orange);
-
-
-alicias_basket.addProduct(bananana);
-alicias_basket.addProduct(orange);
-alicias_basket.addProduct(bapple);
-
-console.log("Pelles basket", pelles_basket.getProducts());
-console.log("Alicias basket", alicias_basket.getProducts());
-
-
-
-pelles_basket.printShoppingInfo();
-alicias_basket.printShoppingInfo();
+exams.getExams().forEach(exam => {
+	console.log(`${exam.getStudent()}'s exam-score is ${exam.getScore()} which results in grade ${exam.getGrade()}.`);
+});
