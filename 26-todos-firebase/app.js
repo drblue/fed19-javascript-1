435b3e0f5440499e1d40de7cff289b7eb27c4cf7
Johan Nordström <johan@digitalvillage.se>
2020-01-13 12:07:29 +0100

    Firebase setup.

---
+++ 26-todos-firebase/app.js
@@ -0,0 +1,61 @@
+/**
+ * My Even Moar Awesomest TODO List.
+ *
+ */
+
+let unfinishedTodosEl = document.querySelector('#unfinished-todos');
+let finishedTodosEl = document.querySelector('#finished-todos');
+let createNewTodoButton = document.querySelector("#createNewTodo");
+
+const createTodoElement = function(todo) {
+	let todoEl = document.createElement('li');
+	todoEl.innerText = todo.getDescription();
+	return todoEl;
+}
+
+const renderTodoList = function() {
+	unfinishedTodosEl.innerHTML = "";
+	finishedTodosEl.innerHTML = "";
+
+	todos.getUnfinishedTodos().forEach(function(todo) {
+		unfinishedTodosEl.append(createTodoElement(todo));
+	});
+
+	todos.getFinishedTodos().forEach(function(todo) {
+		finishedTodosEl.append(createTodoElement(todo));
+	});
+};
+
+// Add click handler for updating completed status
+document.querySelector('#todos').addEventListener('click', function(e) {
+	if (e.target.tagName === "LI") {
+		// find clicked todo
+		const todo = todos.getTodoByDescription(e.target.innerText);
+
+		// update completed status for this todo item
+		todo.toggleCompleted();
+
+		// render the updated todo list to DOM
+		renderTodoList();
+	}
+});
+
+// Add click handler for creating a new TODO
+createNewTodoButton.addEventListener('click', function() {
+	let text = prompt("What do you want to add to the TODO list?", "Do Rainman Dance");
+
+	let todo = new Todo(text);
+
+	// add todo to list of todos
+	todos.addTodo(todo);
+
+	// sort todos alphabetically
+	todos.sortTodos();
+
+	// render the updated todo list to DOM
+	renderTodoList();
+});
+
+// create empty todos-list
+let todos = new TodoList();
+renderTodoList();
---
+++ 26-todos-firebase/todo-classes.js
@@ -0,0 +1,70 @@
+/**
+ * Todo-classes.
+ *
+ */
+
+class TodoList {
+	constructor() {
+		this.todos = [];
+	}
+
+	addTodo(todo) {
+		this.todos.push(todo);
+	}
+
+	getTodoByDescription(description) {
+		return this.todos.find(todo => todo.description === description);
+	}
+
+	getTodos() {
+		return this.todos;
+	}
+
+	getFinishedTodos() {
+		return this.todos.filter(todo => todo.completed);
+	}
+
+	getUnfinishedTodos() {
+		return this.todos.filter(todo => !todo.completed);
+	}
+
+	sortTodos() {
+		// sort todos alphabetically
+		this.todos.sort((a,b) => {
+			if (a.description > b.description) {
+				return 1;
+			} else if (a.description < b.description) {
+				return -1;
+			}
+			return 0;
+		});
+		return this;
+	}
+}
+
+class Todo {
+	constructor(description) {
+		this.description = description;
+		this.completed = false;
+	}
+
+	getDescription() {
+		return this.description;
+	}
+
+	isCompleted() {
+		return this.completed;
+	}
+
+	setCompleted() {
+		this.completed = true;
+	}
+
+	setIncomplete() {
+		this.completed = false;
+	}
+
+	toggleCompleted() {
+		this.completed = !this.completed;
+	}
+}
