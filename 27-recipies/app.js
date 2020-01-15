b1d9224fb04d710adf512f049d5a3536e2304e6f
Johan Nordström <johan@digitalvillage.se>
2020-01-15 10:53:30 +0100

    Refactored to addRecipieToList()-function.

--- 27-recipies/app.js
+++ 27-recipies/app.js
@@ -9,14 +9,15 @@ const db = firebase.firestore();
 // get reference to #recipies
 const recipiesEl = document.querySelector('#recipies');
 
+const addRecipieToList = (recipie) => {
+	recipiesEl.innerHTML += `<li>${recipie.title} (a year ago)</li>`;
+};
+
 db.collection('recipies').get()
 	.then(snapshot => {
 		// loop over the documents in the snapshot
 		snapshot.docs.forEach(doc => {
-			const recipie = doc.data();
-			console.log("recipie title", recipie.title);
-
-			recipiesEl.innerHTML += `<li>${recipie.title} (a year ago)</li>`;
+			addRecipieToList(doc.data());
 		});
 	})
 	.catch(err => {
