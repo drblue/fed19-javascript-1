eb3d0f7ef64026bf4803bfc23634d8a42e0fe50d
Johan Nordström <johan@digitalvillage.se>
2020-01-15 11:44:17 +0100

    Added functionality to create new recipies.

--- 27-recipies/app.js
+++ 27-recipies/app.js
@@ -8,11 +8,39 @@ const db = firebase.firestore();
 
 // get reference to #recipies
 const recipiesEl = document.querySelector('#recipies');
+const newRecipieForm = document.querySelector('#new-recipie');
 
 const addRecipieToList = (recipie) => {
 	const created = moment.unix(recipie.created_at.seconds);
 	recipiesEl.innerHTML += `<li>${recipie.title} (${created.fromNow()})</li>`;
 };
+
+newRecipieForm.addEventListener('submit', e => {
+	// stop form from being submitted
+	e.preventDefault();
+
+	const now = new Date();
+	const recipie = {
+		title: newRecipieForm.recipie_title.value,
+		created_at: firebase.firestore.Timestamp.fromDate(now)
+	};
+
+	// add a new document to the 'recipies' collection
+	db.collection('recipies').add(recipie)
+		.then(res => {
+			console.log("recipie added to collection", res);
+			/**
+			 * @todo
+			 *
+			 * 1. clear input field(s)
+			 * 2. reload recipies from database
+			 * 3. show message that recipie was added successfully
+			 */
+		})
+		.catch(err => {
+			console.error(err);
+		});
+});
 
 db.collection('recipies').get()
 	.then(snapshot => {
