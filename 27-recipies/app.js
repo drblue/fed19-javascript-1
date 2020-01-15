63a80aaed8a7a4cb4bc230e39066d73f37318e31
Johan Nordström <johan@digitalvillage.se>
2020-01-15 10:47:40 +0100

    Get recipies from Firestore collection.

--- 27-recipies/app.js
+++ 27-recipies/app.js
@@ -5,3 +5,20 @@
 
 // connect to firestore
 const db = firebase.firestore();
+
+// get reference to #recipies
+const recipiesEl = document.querySelector('#recipies');
+
+db.collection('recipies').get()
+	.then(snapshot => {
+		// loop over the documents in the snapshot
+		snapshot.docs.forEach(doc => {
+			const recipie = doc.data();
+			console.log("recipie title", recipie.title);
+
+			recipiesEl.innerHTML += `<li>${recipie.title} (a year ago)</li>`;
+		});
+	})
+	.catch(err => {
+		console.error(err);
+	});
