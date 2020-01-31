a02cf03ea9b86dc42d82fa8736b17edfbe3c256e
Johan Nordström <johan@digitalvillage.se>
2020-01-31 13:46:08 +0100

    Symbols.

--- 31-es6-functions/app.js
+++ 31-es6-functions/app.js
@@ -1,50 +1,22 @@
 /**
  * ES6 functions.
  *
- * Sets
+ * Symbols
  */
 
-/*
-// Arrays with duplicate values
-const names = ['Albert', 'Berit', 'Carl', 'David', 'Berit'];
-const numbers = [1, 1, 2, 3, 5, 8, 8, 13, 21];
+const myFirstSymbol = Symbol('I am a symbol');
+const mySecondSymbol = Symbol('I am a symbol');
+console.log('myFirstSymbol:', myFirstSymbol);
+console.log('mySecondSymbol:', mySecondSymbol);
 
-console.log('names:', names);
-console.log('numbers:', numbers);
+// 🤯
+const user = { name: "Cowboy Coder" };
+user.noob_level = 1;
+user['experience level'] = 0; // KIDS - DON'T DO THIS AT HOME!
+user['is-admin'] = false;
 
-// Sets
-// const uniqueArrayValues = arr => {
-// 	return [...new Set(arr)];
-// };
-const uniqueArrayValues = arr => [...new Set(arr)];
+user[Symbol('name')] = 'Mr Cowboy 🤠';
+user[Symbol('name')] = 'Ms Cowgirl 🤠';
 
-const namesSet = new Set(names);
-const numbersSet = new Set(numbers);
 
-console.log('namesSet:', namesSet);
-console.log('numbersSet:', numbersSet);
-
-// const uniqueNames = [...namesSet];
-// const uniqueNames = [...new Set(names)];
-console.log('uniqueNames:', uniqueArrayValues(names));
-console.log('uniqueNumbers:', uniqueArrayValues(numbers));
-
-namesSet.add('Ivan');
-namesSet.add('Ivan');
-namesSet.delete('Albert');
-namesSet.forEach(name => console.log(name));
-
-const ivanExists = namesSet.has('Ivan');
-const albertExists = namesSet.has('Albert');
-console.log("Does namesSet have 'Ivan'?", ivanExists);
-console.log("Does namesSet have 'Albert'?", albertExists);
-*/
-
-const points = new Set();
-points
-	.add(20)
-	.add(25)
-	.add(30)
-	.add(25);
-
-console.log(points);
+console.log(user);
