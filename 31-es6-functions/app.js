de8e9b4f08100235e5d0cb0226ea775ad83369e9
Johan Nordström <johan@digitalvillage.se>
2020-01-31 10:48:38 +0100

    Destructuring.

--- 31-es6-functions/app.js
+++ 31-es6-functions/app.js
@@ -3,3 +3,42 @@
  *
  */
 
+const getWeather = () => {
+	const cityName = "Malmö";
+	const temperature = 8;
+	const humidity = 91;
+	const percipitation = 9;
+
+	return {
+		cityName,
+		temperature,
+		humidity,
+		percipitation
+	};
+}
+
+// const currentWeather = getWeather();
+// const cityName = currentWeather.cityName;
+// const temperature = currentWeather.temperature;
+// const humidity = currentWeather.humidity;
+// const percipitation = currentWeather.percipitation;
+
+const { cityName, temperature, humidity, percipitation } = getWeather();
+console.log(`It's ${temperature} C in ${cityName} right now.`);
+
+
+const getUser = () => {
+	return {
+		username: "jn",
+		name: "Johan Nordström",
+		level: 1337,
+		pets: 2,
+		highscore: 42,
+		is_admin: true,
+	};
+};
+
+const { username, level } = getUser();
+// console.log(user);
+// console.log(`User ${user.username} is at level ${user.level}`);
+console.log(`User ${username} is at level ${level}`);
