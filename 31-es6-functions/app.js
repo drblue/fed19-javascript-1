6e7e5f96fc5e01df2866218f21489f2259a4bdd5
Johan Nordström <johan@digitalvillage.se>
2020-01-31 11:51:16 +0100

    Spread (objects).

--- 31-es6-functions/app.js
+++ 31-es6-functions/app.js
@@ -20,10 +20,24 @@ const sumNumbers = sum(1, 1, 2);
 console.log("Sum of numbers:", sumNumbers);
 
 
-// spread
+// spread (arrays)
 const some_people = ['Albert', 'Berit', 'Carl'];
 const other_people = ['David', 'Erik', 'Frida'];
 
 const people = ['Harald', 'Ivar', ...some_people, ...other_people];
 
-console.log('People', people);
+console.log('People', ...people);
+
+const numbers = [1, 2, 4, 8, 16, 32, 64];
+const more_numbers = [...numbers, 128, 256, 512, 1024, 2048, 4096, 8192, 16384];
+console.log('Numbers:', ...more_numbers);
+
+// spread (objects)
+const albert = { name: "Albert", grade: 4 };
+// const berit = albert;
+// berit.name = "Berit";
+
+const berit = { ...albert, name: "Berit", year: 3 };
+
+console.log('Albert', albert);
+console.log('Berit', berit);
