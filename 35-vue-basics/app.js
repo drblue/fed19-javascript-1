aea71e8413476cf38290ac5f67d0075b23ed9396
Johan Nordström <johan@digitalvillage.se>
2020-02-06 11:35:46 +0100

    Vue directives (attribute binding).

--- 35-vue-basics/app.js
+++ 35-vue-basics/app.js
@@ -6,6 +6,25 @@
 const app = new Vue({
 	el: '#app',
 	data: {
+		name: 'teacher',
 		wish: 'coffee',
+		url: 'https://www.espressohouse.se',
+
+		msg: `This is my message and I'm sticking to it.`,
+
+		msgClass: ['alert', 'alert-warning'],
+	},
+	methods: {
+		getMsgClass() {
+			if (this.msg.includes('DANGER')) {
+				return 'alert alert-danger';
+			} else {
+				return 'alert alert-success';
+			}
+		},
+
+		want() {
+			return `I want ${this.wish}, ${this.name}.`;
+		}
 	},
 });
