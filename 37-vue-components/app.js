69c896518eb67561a5ba6c17417c69a9c44a8635
Johan Nordström <johan@digitalvillage.se>
2020-02-10 10:21:30 +0100

    Vue Components.

---
+++ 37-vue-components/app.js
@@ -0,0 +1,46 @@
+/**
+ * Vue.js Components
+ *
+ */
+
+Vue.component('click-counter', {
+	template: `<button class="btn btn-primary">Clicks: {{ clicks }}</button>`,
+	data() {
+		return {
+			clicks: 0,
+		}
+	}
+});
+
+Vue.component('student', {
+	template: `<li>{{ prefix }} Hello, I am a student</li>`,
+	data() {
+		return {
+			prefix: 'Name:',
+		}
+	}
+});
+
+const app = new Vue({
+	el: '#app',
+
+	data: {
+		// our variables/properties
+		students: [
+			'Adam',
+			'Bertil',
+			'Caesar',
+			'David',
+			'Erik',
+			'Fredrik',
+			'Harald',
+			'Ivar',
+			'Johan'
+		],
+	},
+
+	methods: {
+		// our functions/methods
+
+	},
+});
---
+++ 38-vue-todo-components/app.js
@@ -0,0 +1,77 @@
+/**
+ * Vue.js TODOs
+ *
+ * "Inline" components
+ *
+ */
+
+const app = new Vue({
+	el: '#app',
+
+	data: {
+		// our variables/properties
+		newTodoTitle: '',
+
+		todos: [
+			{
+				title: "Make coffee",
+				completed: true,
+			},
+			{
+				title: "Drink coffee",
+				completed: false,
+			},
+			{
+				title: "Drink MORE coffee",
+				completed: false,
+			},
+			{
+				title: "Do workshop",
+				completed: false,
+			},
+			{
+				title: "Get a room with a nice Vue",
+				completed: true,
+			},
+			{
+				title: "Fire the teacher due to bad puns",
+				completed: false,
+			},
+		],
+	},
+
+	computed: {
+		unfinishedTodos: function() {
+			return this.todos.filter(todo => !todo.completed).length;
+		},
+	},
+
+	methods: {
+		// our functions/methods
+		createTodo() {
+			if (!this.newTodoIsValid()) {
+				return;
+			}
+
+			this.todos.push({
+				title: this.newTodoTitle,
+				completed: false,
+			});
+
+			this.newTodoTitle = '';
+		},
+
+		deleteTodo(todo) {
+			this.todos = this.todos.filter(item => item !== todo);
+		},
+
+		newTodoIsValid() {
+			return (this.newTodoTitle.length > 1);
+		},
+
+		toggleTodo(todo) {
+			todo.completed = !todo.completed;
+		},
+
+	},
+});
