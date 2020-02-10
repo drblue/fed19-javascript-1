f00f2d10ec39310b72d5a3a508f28c96f9313623
Johan Nordström <johan@digitalvillage.se>
2020-02-10 13:27:28 +0100

    Vue CLI demo app boilerplate.

---
+++ 39-vue-cli-app/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="app">
+		<h1>Things To Do</h1>
+
+		<ul>
+			<Todo-Item></Todo-Item>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'App',
+		components: {},
+		data() {
+			return {
+				todos: [
+					{
+						title: 'Learn the Basics about Vue',
+						completed: true,
+					},
+					{
+						title: 'Learn about Vue Components',
+						completed: false,
+					},
+					{
+						title: 'Profit! 🤑',
+						completed: false,
+					},
+				],
+			}
+		},
+	}
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
