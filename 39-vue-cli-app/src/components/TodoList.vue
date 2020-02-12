7b91475004df2cde403662ae02e07e07d3940208
Johan Nordström <johan@digitalvillage.se>
2020-02-12 11:52:42 +0100

    A computed property for unfinished todos.

--- 39-vue-cli-app/src/components/TodoList.vue
+++ 39-vue-cli-app/src/components/TodoList.vue
@@ -1,12 +1,17 @@
 <template>
-	<ul>
-		<Todo-Item
-			v-for="(todo, i) in todos"
-			v-bind:key="i"
-			v-bind:item="todo"
-			v-on:delete-todo="onDeleteTodo($event)"
-		/>
-	</ul>
+	<div class="todo-list">
+		<ul>
+			<Todo-Item
+				v-for="(todo, i) in todos"
+				v-bind:key="i"
+				v-bind:item="todo"
+				v-on:delete-todo="onDeleteTodo($event)"
+			/>
+		</ul>
+		<div class="todo-list-info">
+			Unfinished items: {{ unfinishedTodos }}
+		</div>
+	</div>
 </template>
 
 <script>
@@ -14,10 +19,21 @@ import TodoItem from './TodoItem'
 
 export default {
 	name: 'Todo-List',
+
 	components: {
 		TodoItem,
 	},
+
+	computed: {
+		unfinishedTodos() {
+			// const unfinished = this.todos.filter(todo => !todo.completed);
+			// return unfinished.length;
+			return this.todos.filter(todo => !todo.completed).length;
+		}
+	},
+
 	props: ['todos'],
+
 	methods: {
 		onDeleteTodo(event) {
 			console.log("Todo-List: Want to delete todo with title:", event.todo.title);
