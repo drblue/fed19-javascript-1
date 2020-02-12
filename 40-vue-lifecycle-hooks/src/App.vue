fdbb97674451e5a3c74bf885718849a0ddb63505
Johan Nordström <johan@digitalvillage.se>
2020-02-12 13:04:15 +0100

    Vue Life-Cycle Hooks boilerplate.

---
+++ 40-vue-lifecycle-hooks/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="app">
+		<h1>Vue Life-Cycle Hooks</h1>
+
+		<h2>{{ heading }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	data() {
+		return {
+			heading: 'This is my heading',
+		}
+	},
+}
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
