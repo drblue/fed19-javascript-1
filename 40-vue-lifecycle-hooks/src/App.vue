2f6457cae2234ee7660363fe8cf1c75fc5fbfc0a
Johan Nordström <johan@digitalvillage.se>
2020-02-12 13:47:00 +0100

    Vue Life-Cycle Hooks demo.

--- 40-vue-lifecycle-hooks/src/App.vue
+++ 40-vue-lifecycle-hooks/src/App.vue
@@ -3,16 +3,49 @@
 		<h1>Vue Life-Cycle Hooks</h1>
 
 		<h2>{{ heading }}</h2>
+
+		<button @click="onUpdateHeading()">Update heading</button>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'App',
+
 	data() {
 		return {
 			heading: 'This is my heading',
 		}
+	},
+
+	methods: {
+		onUpdateHeading() {
+			this.heading = 'This is my UPDATED heading';
+		},
+	},
+
+	beforeCreate() {
+		console.log("beforeCreate: " + this.heading);
+	},
+
+	created() {
+		console.log("created: " + this.heading);
+	},
+
+	beforeMount() {
+		console.log("beforeMount: " + this.heading);
+	},
+
+	mounted() {
+		console.log("mounted: " + this.heading);
+	},
+
+	beforeUpdate() {
+		alert("beforeUpdate: " + this.heading);
+	},
+
+	updated() {
+		alert("updated: " + this.heading);
 	},
 }
 </script>
