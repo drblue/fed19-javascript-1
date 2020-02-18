51ee3db35e77f9bea1d1b877b5fa20bcd7791e24
Johan Nordström <johan@digitalvillage.se>
2020-02-18 12:14:51 +0100

    Events and `this` in methods.

--- 42-react-basics/src/UserProfile.js
+++ 42-react-basics/src/UserProfile.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react'
 import EmailLink from './EmailLink'
 
 class UserProfile extends Component {
+
+	state = {
+		greeting: "says hello",
+	}
+
+	// handleClick(e) {
+	handleClick = (e) => {
+		console.log('this in handleClick:', this);
+		console.log('greeting:', this.state.greeting);
+	}
+
 	render() {
+		console.log('this in render:', this);
+
 		const { name, age, email } = this.props.user;
 		return (
 			<div className="user-profile">
 				<div>Name: {name}</div>
 				<div>Age: {age}</div>
 				<div>E-mail: <EmailLink email={email} /></div>
+
+				<p>{this.state.greeting}</p>
+
+				<button onClick={this.handleClick} className="btn btn-info">Say hello</button>
 			</div>
 		)
 	}
