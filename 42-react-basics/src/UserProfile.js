254f29bbf3576b784a09ce56ce035848933c3230
Johan Nordström <johan@digitalvillage.se>
2020-02-18 13:41:45 +0100

    More events.

--- 42-react-basics/src/UserProfile.js
+++ 42-react-basics/src/UserProfile.js
@@ -7,10 +7,20 @@ class UserProfile extends Component {
 		greeting: "says hello",
 	}
 
+	handleCopy = e => {
+		e.preventDefault();
+		alert('No copying, thief!');
+	}
+
 	// handleClick(e) {
 	handleClick = (e) => {
 		console.log('this in handleClick:', this);
 		console.log('greeting:', this.state.greeting);
+	}
+
+	handleMouseOver = e => {
+		console.log('handleMouseOver', e.target);
+		e.target.style.fontWeight = 'bold';
 	}
 
 	render() {
@@ -19,11 +29,11 @@ class UserProfile extends Component {
 		const { name, age, email } = this.props.user;
 		return (
 			<div className="user-profile">
-				<div>Name: {name}</div>
+				<div onMouseOver={this.handleMouseOver}>Name: {name}</div>
 				<div>Age: {age}</div>
 				<div>E-mail: <EmailLink email={email} /></div>
 
-				<p>{this.state.greeting}</p>
+				<p onCopy={this.handleCopy}>{this.state.greeting}</p>
 
 				<button onClick={this.handleClick} className="btn btn-info">Say hello</button>
 			</div>
