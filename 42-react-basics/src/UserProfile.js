9e729c236a92739f1e271bec150c6fa467004a5c
Johan Nordström <johan@digitalvillage.se>
2020-02-18 14:14:59 +0100

    Changing State.

--- 42-react-basics/src/UserProfile.js
+++ 42-react-basics/src/UserProfile.js
@@ -4,36 +4,33 @@ import EmailLink from './EmailLink'
 class UserProfile extends Component {
 
 	state = {
-		greeting: "says hello",
-	}
-
-	handleCopy = e => {
-		e.preventDefault();
-		alert('No copying, thief!');
+		hasSaidHello: false,
 	}
 
 	// handleClick(e) {
 	handleClick = (e) => {
-		console.log('this in handleClick:', this);
-		console.log('greeting:', this.state.greeting);
-	}
+		const greeting = this.state.hasSaidHello
+			? 'says goodbye'
+			: 'says hello'
 
-	handleMouseOver = e => {
-		console.log('handleMouseOver', e.target);
-		e.target.style.fontWeight = 'bold';
+		alert(`${this.props.user.name} ${greeting}`);
+
+		// DON'T DO THIS!!
+		// this.state.greeting = "says goodbye"
+
+		// DO THIS!
+		this.setState({
+			hasSaidHello: !this.state.hasSaidHello,
+		});
 	}
 
 	render() {
-		console.log('this in render:', this);
-
 		const { name, age, email } = this.props.user;
 		return (
 			<div className="user-profile">
-				<div onMouseOver={this.handleMouseOver}>Name: {name}</div>
+				<div>Name: {name}</div>
 				<div>Age: {age}</div>
 				<div>E-mail: <EmailLink email={email} /></div>
-
-				<p onCopy={this.handleCopy}>{this.state.greeting}</p>
 
 				<button onClick={this.handleClick} className="btn btn-info">Say hello</button>
 			</div>
