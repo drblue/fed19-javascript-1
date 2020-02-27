49f5489cb55aca9961229911676f5db882037fcc
Johan Nordström <johan@digitalvillage.se>
2020-02-27 10:04:40 +0100

    Retrieving a single Todo in the new SingleTodo component.

--- 45-react-todo-firestore/src/components/SingleTodo.js
+++ 45-react-todo-firestore/src/components/SingleTodo.js
@@ -1,15 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { db } from '../modules/firebase';
 
-const SingleTodo = (props) => {
-	console.log(props);
+class SingleTodo extends React.Component {
 
-	return (
-		<div className="singleTodo">
-			<h2>This is a single todo</h2>
+	state = {
+		todo: null,
+	}
 
-			You want to see the todo with the ID {props.match.params.id}!
-		</div>
-	)
+	componentDidMount() {
+		db.collection('todos').doc(this.props.match.params.id).get()
+		.then(doc => {
+			// do stuff with document
+			console.log("Got response from Firestore");
+			this.setState({
+				todo: {
+					id: doc.id,
+					title: doc.data().title,
+					completed: doc.data().completed,
+				}
+			});
+			console.log("Todo-state is updated");
+
+		}).catch(err => {
+			console.error(err);
+		});
+	}
+
+	render() {
+		const content = this.state.todo
+			? (
+				<div className="todo-content">
+					<h2>{this.state.todo.title}</h2>
+
+					<p>
+						You want to see the todo with the ID {this.props.match.params.id}!
+					</p>
+
+					<div className="mt-3">
+						<Link to='/' className="btn btn-secondary">&laquo; Back to list</Link>
+					</div>
+				</div>
+			)
+			: (
+				<h2>Loading...</h2>
+			);
+
+		console.log("Rendering component...");
+		return (
+			<div className="singleTodo">
+				{content}
+			</div>
+		)
+	}
 }
 
 export default SingleTodo;
