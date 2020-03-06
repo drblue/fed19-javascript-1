2831f56c77cef234f53ac3f5ca1c5ab21aaffe0e
Johan Nordström <johan@digitalvillage.se>
2020-03-06 11:52:28 +0100

    Dynamically add form fields.

--- 46-react-todo-auth/src/components/todos/AddTodo.js
+++ 46-react-todo-auth/src/components/todos/AddTodo.js
@@ -4,6 +4,7 @@ class AddTodo extends React.Component {
 	state = {
 		title: '',
 		description: '',
+		steps: [""],
 	}
 
 	handleFormSubmit = (e) => {
@@ -14,6 +15,33 @@ class AddTodo extends React.Component {
 		this.setState({
 			title: '',
 			description: '',
+		});
+	}
+
+	handleAddStepClick = e => {
+		const steps = this.state.steps;
+		steps.push("");
+
+		this.setState({
+			steps
+		});
+	}
+
+	handleDeleteStepClick = i => {
+		const steps = this.state.steps;
+		steps.splice(i, 1);
+
+		this.setState({
+			steps
+		});
+	}
+
+	handleInputStepChange = (e, i) => {
+		const steps = this.state.steps;
+		steps[i] = e.target.value;
+
+		this.setState({
+			steps,
 		});
 	}
 
@@ -29,19 +57,20 @@ class AddTodo extends React.Component {
 				<h2>Create a new Todo</h2>
 
 				<div className="form-group">
+					<label htmlFor="title">Title</label>
 					<input
 						type="text"
 						id="title"
 						aria-label="Title of new TODO"
 						placeholder="Type your new todo..."
 						className="form-control"
-						required
 						onChange={this.handleInputChange}
 						value={this.state.title}
 					/>
 				</div>
 
 				<div className="form-group">
+					<label htmlFor="description">Description</label>
 					<textarea
 						id="description"
 						aria-label="Description of new TODO"
@@ -52,7 +81,33 @@ class AddTodo extends React.Component {
 					></textarea>
 				</div>
 
-				<button type="submit" className="btn btn-success w-100">Create</button>
+				<div className="steps-wrapper" className="mb-4">
+					<h3>Steps</h3>
+					{
+						this.state.steps.map((step, i) => (
+							<div className="input-group" key={i}>
+								<input
+									type="text"
+									className="form-control"
+									onChange={e => { this.handleInputStepChange(e, i) }}
+									aria-label={`Type step ${i+1}`}
+									placeholder={`Type step ${i+1}`}
+									value={step}
+								/>
+								<div className="input-group-append">
+									<button className="btn btn-danger" onClick={e => { this.handleDeleteStepClick(i)}}>🗑</button>
+								</div>
+							</div>
+						))
+					}
+					<button className="btn btn-primary" onClick={this.handleAddStepClick}>Add Step</button>
+				</div>
+
+				<button
+					type="submit"
+					className="btn btn-success w-100"
+					disabled={this.state.title ? '' : 'disabled'}
+				>Create</button>
 			</form>
 		)
 	}
